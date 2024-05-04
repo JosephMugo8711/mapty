7d881b893056c2bd0227d4c07b6a0d909ec39031
JosephMugo8711 <wamiti8711@gmail.com>
2024-05-04 18:06:14 +0300

    sidebar

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+
+const Copyright = () => {
+  return (
+    <div>
+        <p className="mt-auto fs-[1.3rem] text-center text-gray-400">
+            &copy; Copyright by
+            <a className="twitter-link pl-1 underline" href="#">
+            Mr. Jowamu
+            </a>
+            . Record and view workouts on a map, adding and tracking multiple workouts with ease..
+        </p>
+      
+    </div>
+  )
+}
+
+export default Copyright
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,17 @@
+import Logo from "./Logo";
+import Copyright from "./Copyright";
+
 const Sidebar = () => {
     return (
-      <div className='flex flex-col h-[855px] w-1/5 bg-[#2d3439] p-12' style={{ overscrollBehaviorY: 'none' }}>
-        <div className='flex-1 text-white'>
-            Logo
-        </div>
-        <div className='text-white'>Copyright</div>
+      <div className='flex flex-col h-[855px] w-[500px] bg-[#2d3439] p-12' style={{ overscrollBehaviorY: 'none' }}>
+         
+         <div className="flex-1">
+            <Logo/>
+         </div>
+         <div>
+           <Copyright />
+         </div>
+         
       </div>
     );
   };
